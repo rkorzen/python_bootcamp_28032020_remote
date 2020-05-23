ab26e68c6cd0a16a7f8fb9103366fbe11ca25ead
rkorzen <korzeniewski@gmail.com>
2020-05-23 12:59:27 +0200

    05 - moduly

---
+++ 05_moduly/mathematica_projekct/mathematica/geometry/figures.py
@@ -0,0 +1,6 @@
+
+def square_area(a: int) -> int:
+    return a  ** 2
+
+def triangle_area(a: int, h: int) -> float:
+    return 0.5 * a * h
--- 05_moduly/mathematica_projekct/tests/test_algebra.py
+++ 05_moduly/mathematica_projekct/tests/test_algebra.py
@@ -9,3 +9,13 @@ def test_add_matrcices():
     expected = [[3, 4],
                 [5, 6]]
     assert add_matrices(a, b) == expected
+
+def test_sub_matrcices():
+    a = [[1, 2],
+         [3, 4]]
+    b = [[2, 2],
+         [2, 2]]
+
+    expected = [[-1, 0],
+                [ 1, 2]]
+    assert sub_matrices(a, b) == expected
